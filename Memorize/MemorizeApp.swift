c457d036cb21dd1bbade4ca19833365cda89fbc5
Lawrenauh <laputa6616@gmail.com>
2025-07-12 17:11:23 +0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Lawrence Hu on 2025/7/12.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
